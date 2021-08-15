3019aea03a13c2079ef1c7cd01a601eb59be9f43
thombruce <thombruce@users.noreply.github.com>
2021-08-15 02:16:53 +0000

    deploy: 892379216405139fa7d50ec18c67852dea307899

--- _nuxt/static/1628971170/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628971170",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"62ba0c1f"}}}}(null,"\u002F"));
